2333aeb420616816c3b570910e7f532241ea24ff
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-06-15 20:07:23 +0430

    add css-module to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./about.module.css";
 
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1 className={styles.title}>About Page</h1>
       <Link to={`/`}>Home </Link>
       <Link to={`/about`}>About </Link>
       <Link to={`/contact`}>Contact </Link>
-      <p>
+      <p className={styles.subTitle}>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis ea
         adipisci consectetur doloribus dolore cumque dolores voluptate quos
         expedita, inventore optio non quo possimus nesciunt et beatae! Dolores,
