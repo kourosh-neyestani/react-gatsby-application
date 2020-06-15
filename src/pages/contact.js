344343364579779389bef3dd3f8c29da005b6d03
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-06-15 20:38:37 +0430

    add SCSS helpers packages, defince used package on gatsby-config and link it

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from "./contact.module.scss"
 
 export default () => (
-  <div>
-    <h1>Contact Me</h1>
+  <div className={styles.contact}>
+    <h1 className={styles.title}>Contact Me</h1>
     <Link to={`/`}>Home </Link>
     <Link to={`/about`}>About </Link>
     <Link to={`/contact`}>Contact </Link>
-    <p>
+    <p className={styles.subTitle}> 
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero rem
       laboriosam voluptatem ea explicabo quidem vitae delectus tempora maiores
       magnam tenetur facilis corporis at quam illo, similique repellendus porro
