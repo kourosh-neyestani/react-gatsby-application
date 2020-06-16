8c2665f9146d9a20ad8f5d63bd869603379c2f76
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2020-06-16 13:42:01 +0430

    add siteMetadata to gatsby-config file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <Layout>
-      <h1>Hello world!</h1>
+      <h1>{data.site.siteMetadata.title}</h1>
     </Layout>
   )
 }
+
+export const query = graphql `{
+  site {
+   siteMetadata {
+     title
+     siteUrl
+     description
+   }
+  }
+}`
